c85a84e0a56b3a9f0e474c7db53a4f17d26a6e91
Sokol8807 <sokol8807@gmail.com>
2022-06-01 17:24:35 +0300

    Initial Commit

---
+++ Tutors/TutorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorsApp.swift
+//  Tutors
+//
+//  Created by Ilya Sokolov on 01.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TutorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
